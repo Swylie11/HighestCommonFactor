cf6ff07e2d0d00bc2678ef4fe9d96d6659731193
Swylie11 <sam.james.wylie@gmail.com>
2023-09-23 09:35:34 +0100

    Added a higher number function and comments

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-#highest common factor
-#Samuel Wylie 12/9/2023
+# highest common factor
+# Samuel Wylie 12/9/2023
 
 choice1 = int(input('What is the first number? '))
 choice2 = int(input('What is the second number? '))
 
 
-def highestcommonfactor(num1, num2):
+def higherNum(num1, num2):
+    # Finds which number is highest out of the two numbers
+    if (num1 - num2) < (num2 - num1):
+        lowerNumber = num1
+    else:
+        lowerNumber = num2
+
+    return lowerNumber
+
+
+def highestCommonFactor(num1, num2):
+    # calculates the highest common factor of the two args
 
     temporary = 1
     originalNum1 = num1
     originalNum2 = num2
 
     while True:
+        # constantly subtracts the smallest number from the largest while they do not equal each other
 
-        # Finds which number is highest out of the two numbers
-        if (num1 - num2) < (num2 - num1):
-            lowernumber = num1
-        else:
-            lowernumber = num2
-
-        if lowernumber == num1:
+        if higherNum(num1, num2) == num1:
             temporary = num1
             num1 = num2
             num2 = temporary
@@ -32,4 +38,4 @@ def highestcommonfactor(num1, num2):
     print(f'Highest common factor of {originalNum1} and {originalNum2} is {num1}')
 
 
-highestcommonfactor(choice1, choice2)
+highestCommonFactor(choice1, choice2)
